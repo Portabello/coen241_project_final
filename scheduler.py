c26ef5e5206a2a268185393250ba6205fc41fe15
wuyuz99 <isaac.wuyuz@gmail.com>
2021-11-29 02:02:03 -0800

    fixed mongodb commands

--- scheduler.py
+++ scheduler.py
@@ -13,15 +13,18 @@ class scheduler:
     def scan(self):
         cursors = self.client.links.links.find()
         for cursor in cursors:
-            if self.client.links.visited.find_one({'_id': cursor.link}):
+            link = cursor["link"]
+            if self.client.links.visited.find_one({'_id': link}):
+                print(link, "already visited")
                 self.client.links.links.delete(cursor)
                 continue
-            self.client.links.visited.insertOne({'_id': cursor.link})
-            domain = urlparse(cursor.link).netloc
+            self.client.links.visited.insertOne({'_id': link})
+            domain = urlparse(link).netloc
             if domain not in self.domains:
+                print(link, "not in domain")
                 continue
-            self.channel.basic_publish(exchange='', routing_key=domain, body = cursor.link)
-            print(cursor.link, "added to mq")
+            self.channel.basic_publish(exchange='', routing_key=domain, body = link)
+            print(link, "added to mq")
             self.client.links.links.delete(cursor)
 
     def __del__(self):
